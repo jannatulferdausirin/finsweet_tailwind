9bf4af11cf4d9dea0a9cd237a64b7140f717d65f
jannatulferdous <jannatulferdausirin1@gmail.com>
2023-10-04 18:25:42 +0300

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,49 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                bg_color : '#1C1E53',
+                nav_color: '#1C1E53',
+                gray: '#BBBBCB',
+                white : '#FFFFFF',
+                yellow : '#FCD980',
+                black : '#1B1C2B',
+                light_gray: '#FFFFFF0D',
+                overly_color : '#1C1E5380',
+                dark_blue_color : '#282938',
+                light_gray : '#F4F6FC',
+                light_blue : '#5239FA',
+
+
+
+
+
+
+
+
+
+
+            }
+        },
+        fontFamily: {
+            'primary_fonts': [
+            'Poppins',
+            'sans - serif'
+
+            ],
+            'secondary_fonts': ['Manrope', 'sans - serif']
+
+
+        },
+backgroundImage : {
+'inquiry_image' : "url('../images/woman-writing-on-paper-3228878.png')",
+
+    
+}
+
+
+
+
+
+    }
+}
